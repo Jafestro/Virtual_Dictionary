4bf54f59990d7469a5419d3cebb6e92bf51475f1
Jafestro <jafestro@gmail.com>
2024-02-16 00:15:07 +0200

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import view.DictionaryView;
 
 public class Main {
+    //Application Lunch
     public static void main(String[] args) {
         Application.launch(DictionaryView.class);
     }
